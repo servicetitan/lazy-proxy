accf20802c14deeb3dd1b93bd549592874900c04
Sergei Pavlov <spavlov@servicetitan.com>
2019-05-10 17:23:39 -0700

    : Use Tailcall optimization

--- LazyProxy/LazyProxyBuilder.cs
+++ LazyProxy/LazyProxyBuilder.cs
@@ -220,6 +220,7 @@ namespace LazyProxy
                     generator.Emit(OpCodes.Ldarg, i);
                 }
 
+                generator.Emit(OpCodes.Tailcall);
                 generator.Emit(OpCodes.Callvirt, method);
                 generator.Emit(OpCodes.Ret);
 
